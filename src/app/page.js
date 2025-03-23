27c1ca59986cce253038ff53d2506b9562a81921
Hiroshana <hiroshanawanni@gmail.com>
2025-03-23 21:29:26 +1100

    Added slider

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Menu from "@/components/Header/Menu/Menu";
 import TopNav from "@/components/Header/TopNav/TopNav";
+import Service from "@/components/Service/Service";
+import Slider from "@/components/Slider/Slider";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +11,10 @@ export default function Home() {
           <TopNav/>
           <Menu/>
       </header>
+      <main className="content">
+        <Slider/>
+        <Service/>
+      </main>
     </div>
   );
 }
